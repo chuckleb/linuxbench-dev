fa4f984187167edaa84e49fb566561daf21031ed
Charles Nguyen <chuckleb@gmail.com>
2015-08-29 17:10:24 -0500

    remove using shm

--- firefox-compile.sh
+++ firefox-compile.sh
@@ -21,7 +21,6 @@ apt-get -y install build-essential libx11-dev libglu-dev hardinfo sysbench unzip
 		php5-curl php5-common php5-cli php5-gd libfpdi-php gfortran wget cmake libncurses5-dev curl \
 		python gtk+-2.0 dbus yasm libasound2-dev libxt-dev zip
 
-cd /dev/shm
 wget https://archive.mozilla.org/pub/firefox/releases/40.0/source/firefox-40.0.source.tar.bz2
 tar xvfj firefox-40.0.source.tar.bz2
 mkdir -p ffbuild; cd ffbuild
